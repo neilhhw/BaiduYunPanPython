7197ee015c4ec5705b6c74242ad099dc7d63e150
neilhhw <neilhhw@gmail.com>
2014-01-07 14:09:52 +0800

    Add configuration UI basic

--- UI.py
+++ UI.py
@@ -1,5 +1,45 @@
 import wx
 
+
+class ConfFrame( wx.Frame ):
+	def __init__( self, parent ):
+		wx.Frame.__init__( self, parent, id = wx.ID_ANY, title = u"FileSync UI Configuration",
+                           pos = wx.DefaultPosition, size = wx.Size(600, 480),
+                           style = wx.DEFAULT_FRAME_STYLE|wx.TAB_TRAVERSAL )
+
+		self.SetSizeHintsSz( wx.DefaultSize, wx.DefaultSize )
+
+		bSizer1 = wx.BoxSizer( wx.VERTICAL )
+
+		self.ConfTabPanel = wx.Notebook( self, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, 0 )
+		self.FolderPanel = wx.Panel( self.ConfTabPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		bSizer5 = wx.BoxSizer( wx.VERTICAL )
+
+		self.m_listbook2 = wx.Listbook( self.FolderPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.LB_DEFAULT )
+
+		bSizer5.Add( self.m_listbook2, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		self.FolderPanel.SetSizer( bSizer5 )
+		self.FolderPanel.Layout()
+		bSizer5.Fit( self.FolderPanel )
+		self.ConfTabPanel.AddPage( self.FolderPanel, u"Folders", True )
+		self.m_panel10 = wx.Panel( self.ConfTabPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.ConfTabPanel.AddPage( self.m_panel10, u"a page", False )
+		self.m_panel11 = wx.Panel( self.ConfTabPanel, wx.ID_ANY, wx.DefaultPosition, wx.DefaultSize, wx.TAB_TRAVERSAL )
+		self.ConfTabPanel.AddPage( self.m_panel11, u"a page", False )
+
+		bSizer1.Add( self.ConfTabPanel, 1, wx.EXPAND |wx.ALL, 5 )
+
+
+		self.SetSizer( bSizer1 )
+		self.Layout()
+
+		self.Centre( wx.BOTH )
+
+	def __del__( self ):
+		pass
+
 TRAY_TOOLTIP = 'File Sync for Baidu Cloud'
 TRAY_ICON = 'icon.png'
 
@@ -15,6 +55,7 @@ class TaskBarIcon(wx.TaskBarIcon):
         super(TaskBarIcon, self).__init__()
         self.set_icon(TRAY_ICON)
         self.Bind(wx.EVT_TASKBAR_LEFT_DOWN, self.on_left_down)
+        self.confFrame = ConfFrame(parent=None)
 
     def CreatePopupMenu(self):
         menu = wx.Menu()
@@ -31,9 +72,8 @@ class TaskBarIcon(wx.TaskBarIcon):
         print 'Tray icon was left-clicked.'
 
     def on_config(self, event):
-        print 'Hello, world!'
-        frame = wx.Frame(parent=None, title='File Sync for Baidu Cloud')
-        frame.Show()
+        print 'Open configuration Form'
+        self.confFrame.Show(True)
 
     def on_exit(self, event):
         wx.CallAfter(self.Destroy)
