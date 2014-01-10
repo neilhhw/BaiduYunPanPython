dc6e18de7955bc4ec38e869155dfb8f3cb65d0ea
neilhhw <neilhhw@gmail.com>
2014-01-10 16:03:24 +0800

    Change Log format, add API abstract class

--- lib/common/LogManager.py
+++ lib/common/LogManager.py
@@ -13,7 +13,7 @@ logging.basicConfig(level=logging.DEBUG,
 #定义一个StreamHandler，将INFO级别或更高的日志信息打印到标准错误，并将其添加到当前的日志处理对象#
 console = logging.StreamHandler()
 console.setLevel(logging.DEBUG)
-#formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
-#console.setFormatter(formatter)
+formatter = logging.Formatter('%(asctime)s %(filename)s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
 logging.getLogger('').addHandler(console)
 ################################################################################################
