a058a966928303673c605cf1020206c3bc42ce41
hhou <hongwei.hou@nsn.com>
2014-02-13 11:40:39 +0800

    Modify UI, make close popup dialog does not exit the program

--- lib/ui/TestUI.py
+++ lib/ui/TestUI.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt4.QtGui import QApplication, QWidget, QSystemTrayIcon, QIcon, QMenu, QAction, QFrame
+from PyQt4.QtGui import QApplication, QWidget, QSystemTrayIcon, QIcon, QMenu, QAction, QDialog, QLayout
 from PyQt4.QtCore import QString, SIGNAL, SLOT
-from UniFileSyncPop import Ui_Form
+from UniFileSyncPop import Ui_UniFileSyncPop
 
 try:
     _fromUtf8 = QString.fromUtf8
@@ -10,22 +10,29 @@ except AttributeError:
         return s
 
 
-
 app = QApplication(sys.argv)
 
 m = QWidget()
-
-ui = Ui_Form()
-
-ui.setupUi(m)
+d = QDialog(m)
 
 t = QSystemTrayIcon(m)
 t.setIcon(QIcon('icon.png'))
 t.showMessage(_fromUtf8('UniFileSyncPop Message'), _fromUtf8('This is UniFileSyncUI!!'))
 
+ui = Ui_UniFileSyncPop()
+ui.setupUi(d)
+
+def func():
+    """docstring for func"""
+    print 'I am OK'
+
+ui.connBtn.connect(ui.connBtn, SIGNAL('clicked()'), func)
+
+d.setFixedSize(d.size())
+
 menu = QMenu(m)
 es = menu.addAction(_fromUtf8('ShowConfig'))
-menu.connect(es, SIGNAL('triggered()'), m, SLOT('show()'))
+menu.connect(es, SIGNAL('triggered()'), d, SLOT('show()'))
 
 ea = menu.addAction(_fromUtf8('Exit'))
 menu.connect(ea, SIGNAL('triggered()'), app, SLOT('quit()'))
@@ -33,5 +40,7 @@ menu.connect(ea, SIGNAL('triggered()'), app, SLOT('quit()'))
 t.setContextMenu(menu)
 
 t.show()
+
+app.setQuitOnLastWindowClosed(False)
 
 sys.exit(app.exec_())
