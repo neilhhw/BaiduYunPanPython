61057443a23ececba46a981b8d49c7ef767bc624
hhou <hongwei.hou@nsn.com>
2014-03-03 11:50:45 +0800

    Fix save proxy problem

--- lib/common/Net.py
+++ lib/common/Net.py
@@ -37,11 +37,11 @@ def set_proxy(proxies, **kargs):
     proxyHandler = urllib2.ProxyHandler(proxies)
 
     for h in __handlers:
-        if type(h) == urllib2.ProxyHandler:
-            logging.debug('[Net]: find previous proxy handler, remove it')
+        if isinstance(h, urllib2.ProxyHandler):
+            #logging.debug('[Net]: find previous proxy handler, remove it')
             __handlers.remove(h)
-        elif type(h) == urllib2.ProxyBasicAuthHandler:
-            logging.debug('[Net]: find previous proxy auth handler, remove it')
+        elif isinstance(h, urllib2.ProxyBasicAuthHandler):
+            #logging.debug('[Net]: find previous proxy auth handler, remove it')
             __handlers.remove(h)
 
     if 'username' in kargs and 'password' in kargs:
